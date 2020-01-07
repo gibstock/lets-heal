f92110971768b924285356b6242a480f9fa409e8
gibstock <gibstock@gmail.com>
2020-01-07 02:49:43 -0800

    mantras

--- js/script.js
+++ js/script.js
@@ -22,7 +22,69 @@ const email = document.querySelector('#email');
 let codeArray = ['746374', '948374', '930384', '991711', '938373','99843','988444', '017151', '243794', '726222', '009909'];
 
 let mantras = [
-    'Let the light in', 'give the last bite away', 'stop and pay close attention', 'say HELLO today', 'Be here NOW'
+    'Let the light in', 'give the last bite away', 'stop and pay close attention', 'say HELLO today', 'Be here NOW', 'Life responds to my deep desire to tap into abundance','Thank you. Thank you. Thank you.',
+    'I measure my success by how much fun I’m having.',
+    
+   ' I am open to accepting all forms of abundance that are on their way to me',
+    
+    'Life becomes so giving when I am generous',
+    
+  '  All of life’s truest riches are within my reach',
+    
+    'I have (more than) enough time (for everything that I love)',
+    
+    'I delight in and praise the good fortune and success of others; the spark that burns in them also resides in me',
+    
+    ' I have everything that I need, right here and now, to fully engage with life',
+    
+    'I transform every obstacle into an opportunity',
+    
+    ' I am eternally grateful for the abundant intelligence that brought me here',
+    
+    'In every waking moment, I touch the depth of life and experience my own expansion',
+    
+    ' I am connected to boundless abundance, creativity, and potential',
+    
+    ' I am a magnet for miracles',
+    
+    'I am grateful for every gift that I’ve been given, have now, and have yet to receive',
+    
+    'The universe always has my back, and is supporting my highest good',
+    
+    
+    
+    
+    'I have it in me to figure out what I need to become in order to be where I want to be',
+    
+    ' I am willing to embrace the glorious mess that I am',
+    
+    ' I am shedding the energies that no longer match the frequency of my future',
+    
+    'I love my food, and my food loves me',
+    
+    'I am always doing my best, and I can forgive the rest',
+    
+    ' I imagine the best possible version of me, and I release any part of me that doesn’t believe in it',
+    
+    'As I love and accept myself as I am, right now, life gets better',
+    
+    
+    
+    'It does not matter how slowly I go compared to others; what truly matters is my authentic presence with each step',
+    
+    'I am not lukewarm in my passion, but fully committed to what I want to experience today',
+    
+    'I attract my best possible self by devoting the entirety of my energy to feeling and being what I want',
+    
+    'The moon shines in every phase of her life, and so can I',
+    
+    ' My sincere self-acceptance takes me straight to joy',
+    
+    'I am my own best friend',
+    
+    'All that I have ever loved stays with me',
+    
+    'I am afforded ample time to grow into who I’m destined to be, and I am loved along the way',
 ]
 
 let date = new Date();
